acec98c51ab4f86a8883b25cfc383604d1521e40
Al Jade <aljade1902@gmail.com>
2020-09-11 21:25:28 +0100

    Reading in with pandas.

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ def main():
     print("Welcome to the machine learning program!")
     print("Glass data will be used to classify different types of glass.")
 
+
+def classify():
+    data = pd.read_csv()  # Read the glass data.
+
+
+main()
