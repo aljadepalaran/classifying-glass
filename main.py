68fa86ffd21a0b07e81b3c54c217729dae5baeec
Al Jade <aljade1902@gmail.com>
2020-09-11 21:23:35 +0100

    Pandas added to the project and imported. 'main' function generated.

---
+++ main.py
@@ -0,0 +1,8 @@
+# Importing modules
+import pandas as pd
+
+
+def main():
+    print("Welcome to the machine learning program!")
+    print("Glass data will be used to classify different types of glass.")
+
